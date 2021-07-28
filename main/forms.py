f10be4e24de34a59a10c6846f1e58023413fe031
shashank725 <shashank7rocks@gmail.com>
2021-07-28 23:57:08 +0530

    Added navbar

--- main/forms.py
+++ main/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class CreateNewList(forms.Form):
     name=forms.CharField(label="Name", max_length=100)
-    check=forms.BooleanField()
+    check=forms.BooleanField(required=False)
